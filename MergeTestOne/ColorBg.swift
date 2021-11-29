3cfce649f316531e9daaffc81090e552a6befaa8
thenappan.2010@gmail.com <Thenappan.Ramanathan@walmart.com>
2021-11-29 21:58:10 +0530

    Project added

---
+++ MergeTestOne/ColorBg.swift
@@ -0,0 +1,21 @@
+//
+//  ColorBg.swift
+//  MergeTestOne
+//
+//  Created by Thenappan Ramanathan on 29/11/21.
+//
+
+import Foundation
+import UIKit
+
+public enum colorBG {
+
+    case white
+    case red
+}
+
+
+public struct ColorValue {
+    let white = UIColor.black
+    let blue = UIColor.blue.withAlphaComponent(0.3)
+}
---
+++ MergeTestOne/MergeTestOne/MergeTestOne.h
@@ -0,0 +1,18 @@
+//
+//  MergeTestOne.h
+//  MergeTestOne
+//
+//  Created by Thenappan Ramanathan on 29/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MergeTestOne.
+FOUNDATION_EXPORT double MergeTestOneVersionNumber;
+
+//! Project version string for MergeTestOne.
+FOUNDATION_EXPORT const unsigned char MergeTestOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MergeTestOne/PublicHeader.h>
+
+
